b80173454999775acb69ab8c6fb7b4a729766e90
rzkytmgr <rzkytmgr@gmail.com>
2021-03-24 03:25:41 +0700

    fixing bug deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const router = require("./routes");
-require("dotenv").config();
+const express = require('express');
+const router = require('./routes');
+require('dotenv').config();
 
 const app = express();
-
-app.use("/", router);
+app.use('/', router);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, "localhost", () => {
-  console.log(`Server Running http://localhost:${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server Running http://localhost:${PORT}`);
 });
