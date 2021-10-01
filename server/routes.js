0c38e6417d7bca2812762d110c9152579259a8c1
rzkytmgr <rzkytmgr@gmail.com>
2021-10-01 16:21:39 +0700

    :yarn: Refactoring code :: call the middleware using `use` method from `route`

--- server/routes.js
+++ server/routes.js
@@ -4,12 +4,13 @@ const middleware = require('./middleware');
 const { homeController, imamController, quranController } = require('./controller');
 require('dotenv').config();
 
-router.get('/', middleware, homeController);
-router.get('/imam', middleware, imamController);
-router.get('/imam/:id', middleware, imamController);
-router.get('/quran', middleware, quranController);
-router.get('/quran/:surah', middleware, quranController);
-router.get('/quran/:surah/:ayah', middleware, quranController);
+router.use(middleware);
+router.get('/', homeController);
+router.get('/imam', imamController);
+router.get('/imam/:id', imamController);
+router.get('/quran', quranController);
+router.get('/quran/:surah', quranController);
+router.get('/quran/:surah/:ayah', quranController);
 
 router.get('*', (req, res, next) => res.status(404).json({ status: 404, message: 'nothing here!' }));
 
