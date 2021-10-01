d42a94b1c39fdbcdd67e6071ee65510f839f5ebb
rzkytmgr <rzkytmgr@gmail.com>
2021-10-01 16:22:47 +0700

    :bug: CORS and Request log

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	console.debug('[', req.method, ']', res.statusCode, req.originalUrl);
+	next();
+});
+
 app.use('/', router);
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log(`Server Running http://localhost:${PORT}`);
+	console.clear();
+	console.debug(`Server Running http://localhost:${PORT}`);
 });
 
 module.exports = app;
