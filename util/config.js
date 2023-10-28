4d81356b203bcb035dd004976fa846b7cd7c2d7e
Rizky Aulia Tumangger <rzkytmgr@gmail.com>
2023-10-29 05:18:39 +0700

    initial redeveloped

--- util/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { PORT, BASE_URI } = process.env;
-
-module.exports = {
-	PORT,
-	BASE_URI,
-};
--- util/helper.js
+++
@@ -1,3 +0,0 @@
-const numberFormatter = number => String(number < 10 ? '00' + number : number < 100 ? '0' + number : number);
-const filterParams = params => (isNaN(Number(params)) && typeof params !== 'undefined' ? true : false);
-module.exports = { numberFormatter, filterParams };
