d0f3040993a9207824d90901d736f5f45379c67b
rzkytmgr <rzkytmgr@gmail.com>
2025-05-07 11:24:06 +0700

    🐛 fix(vercel): resolve dynamic import path on vercel

--- src/utils/jsonResolver.ts
+++ src/utils/jsonResolver.ts
@@ -1,6 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 /** This function used for resolve JSON file with imutable behaviour */
 const jsonResolver = async <T>(filename: string | string[]): Promise<T> => {
-  const data = await import(`@db/${Array.isArray(filename) ? filename.join('/') : filename}.db.json`);
+  const data = await import(path.resolve('src', 'db', `${Array.isArray(filename) ? filename.join('/') : filename}.db.json`), {
+    assert: {
+      type: 'json',
+    },
+  });
   return JSON.parse(JSON.stringify(data.default));
 };
 
